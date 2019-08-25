3cd131e8c042d8b84387084a99ab19f8585836bc
Cresence <devharris93@gmail.com>
2019-08-25 10:40:23 -0400

    Added start to npm

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (name) VALUES ('Double Stack Bacon w/ Cheddar');
+INSERT INTO burgers (name) VALUES ('The Impossible Burger');
+INSERT INTO burgers (name, eaten) VALUES ('Big Mac', true);
+INSERT INTO burgers (name, eaten) VALUES ('Double Whopper W/ Cheese', true);
+INSERT INTO burgers (name, eaten) VALUES ('Baconator Jr.', true);
+INSERT INTO burgers (name) VALUES ('Crispy Chicken w/ Cheese');
+
+SELECT * FROM burgers
